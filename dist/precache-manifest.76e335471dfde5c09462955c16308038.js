651f84bf2edf01948af4b236a2b5c01ae34a2363
prakash-ces <prakash.perumalmariappan@cesltd.com>
2018-04-18 20:31:24 +0530

    production ready

---
+++ dist/precache-manifest.76e335471dfde5c09462955c16308038.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ff7eb2e6d57f072c2b4",
+    "url": "/js/vendor.8ff7eb2e.js"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "cbc48bed4efc75a7916c",
+    "url": "/js/app.cbc48bed.js"
+  },
+  {
+    "revision": "6ad99ac631a5469b6a77eb833cc4a7b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbc48bed4efc75a7916c",
+    "url": "/css/app.fbae4076.css"
+  }
+];
